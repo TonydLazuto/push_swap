db814cc98a5501489c1e44b2fcb90af65214e7fe
Tonyd <aderose73@gmail.com>
2021-06-11 08:38:51 +0200

    rename properly

--- bonus/checker.h
+++ bonus/checker.h
@@ -6,7 +6,7 @@
 /*   By: tonyd <aderose73@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 13:26:58 by tonyd             #+#    #+#             */
-/*   Updated: 2021/06/10 09:46:54 by tonyd            ###   ########.fr       */
+/*   Updated: 2021/06/11 08:37:13 by tonyd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,11 +35,14 @@ typedef struct		s_num
 	struct s_num	*next;
 }					t_num;
 
-//int		ft_exit(int *nb);
+//int	ft_exit(t_ins *ins, t_num *nb);
+void	print_ins(t_ins *ins);
 void	ft_error_ins(t_ins **ins, t_num **nb);
 void	ft_error_nb(t_num **nb);
 int		check_instructions(char *line);
 t_ins	*new_ins(char *str);
+t_ins	*fill_stack_ins(t_ins *ins, char *line, t_num *nb);
+void	read_instructions(t_num *nb);
 t_ins	*last_ins(t_ins *ins);
 void	push_back_ins(t_ins **list, t_ins *elet);
 int		my_atoi(char *str, t_num *mynb);
--- bonus/main.c
+++ bonus/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tonyd <aderose73@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 12:52:41 by tonyd             #+#    #+#             */
-/*   Updated: 2021/06/10 09:04:39 by tonyd            ###   ########.fr       */
+/*   Updated: 2021/06/11 08:31:12 by tonyd            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,61 +35,6 @@ void		print_ins(t_ins *ins)
 	{
 		printf("|%s|\n", cpy->str);
 		cpy = cpy->next;
-	}
-}
-
-int		check_instructions(char *line)
-{
-	if (ft_strlen(line) == 3)
-	{
-		if (ft_strncmp(line, "rra", 3) == 0 || ft_strncmp(line, "rrb", 3) == 0
-				|| ft_strncmp(line, "rrr", 3) == 0)
-			return (1);
-	}
-	if (ft_strlen(line) == 2)
-	{
-		if (!ft_strncmp(line, "sa", 2) || !ft_strncmp(line, "sb", 2) || !ft_strncmp(line, "ss", 2)
-	 		|| !ft_strncmp(line, "pa", 2) || !ft_strncmp(line, "pb", 2)
-	 		|| !ft_strncmp(line, "ra", 2) || !ft_strncmp(line, "rb", 2) || !ft_strncmp(line, "rr", 2))
-	 	return (1);
-	}
-	return (0);
-}
-
-t_ins		*fill_stack_ins(t_ins *ins, char *line, t_num *nb)
-{
-	int 	valid;
-	t_ins	*elet;
-
-	elet = NULL;
-	valid = check_instructions(line);
-	if (!valid)
-		ft_error_ins(&ins, &nb);
-	elet = new_ins(line);
-	if (!elet)
-		ft_error_ins(&ins, &nb);
-	push_back_ins(&ins, elet);
-	if (!ins)
-		ft_error_ins(&ins, &nb);
-	return (ins);
-}
-
-void		read_instructions(t_num *nb)
-{
-	int		fd;
-	char	*line;
-	t_ins	*ins;
-	int		res;
-
-	line = NULL;
-	ins = NULL;
-	res = 1;
-	while (res > 0) // voir si fuite memoire si direct si while(gnl())
-	{
-		res = get_next_line(1, &line);
-		if (res > 0)
-			ins = fill_stack_ins(ins, line, nb);
-		free(line);
 	}
 }
 
